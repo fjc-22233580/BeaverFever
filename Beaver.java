aebae3c1f16a78597c57eab1ae2916fa40d8f38a
fjc-22233580 <22233580@bucks.ac.uk>
2023-12-08 21:03:32 +0000

    Added moving logic (inc collision detect)

--- Beaver.java
+++ Beaver.java
@@ -8,12 +8,49 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Beaver extends Actor
 {
+
+    final int VELOCITY = 3;
+
     /**
      * Act - do whatever the Beaver wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
-        // Add your action code here.
+    public void act() {
+        // Up
+        if (Greenfoot.isKeyDown("w")) {
+
+            customSetLocation(getX(), getY() - VELOCITY);
+        }
+
+        // Left
+        if (Greenfoot.isKeyDown("a")) {
+
+            customSetLocation(getX() - VELOCITY, getY());
+        }
+
+        // Down
+        if (Greenfoot.isKeyDown("s")) {
+
+            customSetLocation(getX(), getY() + VELOCITY);
+        }
+
+        // Right
+        if (Greenfoot.isKeyDown("d")) {
+
+            customSetLocation(getX() + VELOCITY, getY());
+        }
+    }
+
+    public boolean customSetLocation(final int x, final int y) {
+        final int oldX = getX();
+        final int oldY = getY();
+        setLocation(x, y);
+        if (isTouching(ObjectTile.class)) {
+
+            // Collided with an object, so revert to previous position.
+            setLocation(oldX, oldY);
+            return false; // move was not allowed!
+        }
+        return true; // move was allowed!
     }
 }
