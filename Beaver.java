41c2090b81f068a485e9346b2b669af96bcccf61
fjc-22233580 <22233580@bucks.ac.uk>
2023-12-08 20:43:37 +0000

    Added nugget(beaver)

---
+++ Beaver.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Beaver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Beaver extends Actor
+{
+    /**
+     * Act - do whatever the Beaver wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
