d40c612b92c64d71b8591b9b1ee8e024007cd260
fjc-22233580 <22233580@bucks.ac.uk>
2023-12-09 15:49:46 +0000

    Added image path importing

--- MapParser.java
+++ MapParser.java
@@ -4,6 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MapParser {
@@ -11,12 +12,14 @@ public class MapParser {
     public MapParser(){
 
         try {
+
             getMapFiles("griddata");
+            getFiles("images");
+
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
     }
 
     private void getMapFiles(String mapFilesPath) throws IOException{
@@ -40,6 +43,31 @@ public class MapParser {
 
         System.out.println("Imported CSVs: " + csvPaths.size());
         System.out.println("Imported CSV: " + csvPaths.get(0));
+    }
+
+    private HashMap<Integer, String> getFiles(String imageTilesPath) throws IOException {
+
+        HashMap<Integer, String> imgPaths = new HashMap<Integer, String>();
+
+        int count = 0;
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(imageTilesPath))) {
+
+            for (Path path : stream) {
+
+                if (Files.isDirectory(path) == false ) {
+
+                    String fileName = path.getFileName().toString();
+
+                    if (HelperMethods.getFileExtension(fileName).equals(".png")) {
+                        imgPaths.put(count, fileName);
+                    }
+                }
+                count++;
+            }
+        }
+
+        return imgPaths;
     }
     
 }
