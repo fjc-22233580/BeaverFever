1613fe32ca92c48c729e2fb45af4db2ee790192e
fjc-22233580 <22233580@bucks.ac.uk>
2023-12-14 21:50:34 +0000

    Added tile id to TileInfo

--- TileInfo.java
+++ TileInfo.java
@@ -2,15 +2,22 @@ public class TileInfo {
 
     private int x;    
     private int y;    
-    private String tilePath;    
+    private String tilePath;
+    private int tileNumber;    
     
-    public TileInfo(int x, int y, String tilePath) {
+    
+    public TileInfo(int tileNumber, int x, int y, String tilePath) {
+        this.tileNumber = tileNumber;
         this.x = x;
         this.y = y;
         this.tilePath = tilePath;
         
     }
-
+    
+    public int getTileNumber() {
+        return tileNumber;
+    }
+    
     public int getX() {
         return x;
     }
