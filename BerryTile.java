02c3a8e5d6760bce7dc9dcce34b61d6e7523e0a6
fjc-22233580 <22233580@bucks.ac.uk>
2023-12-15 22:25:31 +0000

    Added new tiles

---
+++ BerryTile.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BerryTile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BerryTile extends Actor
+{
+
+    public BerryTile(String tilePath) {
+        GreenfootImage tile = new GreenfootImage(tilePath);
+        setImage(tile);
+    }
+
+    /**
+     * Act - do whatever the BerryTile wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ WaterTile.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WaterTile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WaterTile extends Actor
+{
+    public WaterTile(String tilePath) {
+        GreenfootImage tile = new GreenfootImage(tilePath);
+        setImage(tile);
+    }
+    /**
+     * Act - do whatever the WaterTile wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
