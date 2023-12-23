455ad8360752bf6baeb5bf8ffe6a320593f1850c
fjc-22233580 <22233580@bucks.ac.uk>
2023-12-23 21:10:23 +0000

    Completed status bar moving into PlayerStats

--- PlayerStats.java
+++ PlayerStats.java
@@ -1,12 +1,26 @@
+import greenfoot.Color;
+
 public class PlayerStats {
 
     private final int MAX_LIVES = 3;
 
-    private int woodCount;    
+    private int woodCount = 0;    
     private int livesCount;   
+    private StatusBar statusBar = new StatusBar();
+    private Label statusBarLabel = new Label(woodCount, 12); 
     
     public PlayerStats() {
 
+        statusBarLabel.setFillColor(Color.BLACK);
+        statusBarLabel.setLineColor(Label.transparent);
+    }
+
+    public Label getStatusBarLabel() {
+        return statusBarLabel;
+    }
+
+    public StatusBar getStatusBar() {
+        return statusBar;
     }
 
     public int getMAX_LIVES() {
@@ -15,11 +29,20 @@ public class PlayerStats {
 
     public void addWood() {
         woodCount++;
-        System.out.println("Wood count: " + woodCount);
+        statusBarLabel.setValue(woodCount);
+        System.out.println("Wood count: " + woodCount); 
     }
 
-    public void addLife() {
+    public boolean addLife() {
+        boolean lifeAdded = false;
         livesCount++;
+
+        if (livesCount < MAX_LIVES) {
+            livesCount++;
+            lifeAdded = true;
+        }
+
+        return lifeAdded;
     }
     
     public int getWoodCount() {
