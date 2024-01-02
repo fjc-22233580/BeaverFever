e1c58a6aaaa34400f155e7ac74c4266f2ff453f9
fjc-22233580 <22233580@bucks.ac.uk>
2024-01-02 13:16:16 +0000

    tidy

--- WorldManager.java
+++ WorldManager.java
@@ -9,6 +9,8 @@ public class WorldManager {
 
     // #region TODO List:
 
+        // Code:
+
         // 1. Complete - Add counter for health - currently only wood is reported.
         // 2. WIP - Add bridge building mechanism - suggest another key binding that only works if near the river, in one spot, then each press,
         //    builds a portion of the bridge with a small time - can be based off the game cycle iteration. 
@@ -24,7 +26,9 @@ public class WorldManager {
         // 12. BUG - Add logic to enemy so after initial attack, it does a loop of the map, and then attacks again - perhaps instead of delay timer? 
         // 13. Pass the construction point to necessary worlds
 
+
         // ArtWork: 
+
         // (Note: all artwork must either be .png or .gif, have a transparent background, and be reasonably sized to work with the game )
         // 1. Create beaver chopping gif - an axe already exists in the asset pack, suggest combining that with current beaver image to create a gif. 
         // 2. Create the enemy attacking gif.
@@ -34,8 +38,10 @@ public class WorldManager {
         // 6. Create "Bridge" artwork - needs to be same size as current game tiles.
         // 7. Create "Key" artwork - needs to be same size as current game tiles.
         // 8. Create button art work: "Play", "Dev Mode", "Restart"  - needs to be approx size that a button might be, like the thing we have now.
+        
 
         // Map Design:
+
         // 1. Tweak river path in map 7 to have a more horizontal portion - this will help coding the bridge building logic.
         // 2. Move trees in all maps so they are closer to the sides - this will help with the enemy patrol path logic. 
         // 3. BUG - I think map 4 has a berry tile which has been incorrectly assigned as a tree tile - double check IDs in .csv files
