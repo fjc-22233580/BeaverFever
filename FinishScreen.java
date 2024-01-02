2bcf3859bcc3c09fa03e9e6b20bd62f7d61a3f45
fjc-22233580 <22233580@bucks.ac.uk>
2024-01-02 08:37:46 +0000

    Added finish screen

---
+++ FinishScreen.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FinishScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FinishScreen extends World
+{
+
+    /**
+     * Constructor for objects of class FinishScreen.
+     * 
+     */
+    public FinishScreen()
+    {    
+        super(240, 240, 1);
+    }
+}
