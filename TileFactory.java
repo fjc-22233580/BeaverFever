3bc1b916c3aacaaaa22ca21f250cfe903ea71dc3
fjc-22233580 <22233580@bucks.ac.uk>
2024-01-03 12:02:34 +0000

    Added switch for tiles

--- TileFactory.java
+++ TileFactory.java
@@ -2,5 +2,18 @@ public class TileFactory {
     
     public void createTile(String tilePath, ActorType type) {
         // TODO - Create a new ObjectTile and return it
+
+        switch (type) {
+            case BERRY:
+                //eturn new BerryTile(tilePath, type);
+            case WOOD:
+                //return new WoodTile(tilePath, type);
+            case WATER:
+                //return new WaterTile(tilePath, type);
+            case OTHER:
+                //return new ObjectTile(tilePath, type);
+            default:
+                throw new IllegalArgumentException("Invalid tile type");
+        }
     }
 }
