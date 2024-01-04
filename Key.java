2f60c68718b7e69031936f5e0b11484722f715f4
fjc-22233580 <22233580@bucks.ac.uk>
2024-01-04 18:02:18 +0000

    Added ctor

--- Key.java
+++ Key.java
@@ -1,19 +1,18 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Key here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Key class, used to unlock the castle. 
  */
-public class Key extends Actor
-{
+public class Key extends Actor {
+
+    public Key() {
+    }
+
     /**
      * Act - do whatever the Key wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
+    public void act() {
         // Add your action code here.
     }
 }
