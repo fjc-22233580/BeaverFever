6bd2a6ee03eb7844401156bc2347a2a2e840b7fe
TESmith01 <147707504+TESmith01@users.noreply.github.com>
2024-01-04 22:12:28 +0000

    added key image 22:12

--- Key.java
+++ Key.java
@@ -6,6 +6,8 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Key extends Actor {
 
     public Key() {
+        GreenfootImage newkeyv4 = new GreenfootImage("new key v4.png");
+        setImage(newkeyv4);
     }
 
     /**
@@ -13,6 +15,6 @@ public class Key extends Actor {
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() {
-        // Add your action code here.
+
     }
 }
