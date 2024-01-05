c6d7d5eaea0c84eae76c3fca5aa7540ce880a7b8
fjc-22233580 <22233580@bucks.ac.uk>
2024-01-05 13:44:33 +0000

    Added princess class.

---
+++ PrincessOlive.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+public class PrincessOlive extends Actor {
+
+
+    public PrincessOlive() {
+        GreenfootImage image = new GreenfootImage("Happy Princess.png");
+        setImage(image);
+    }
+
+    public void winGame(){
+        GreenfootImage image = new GreenfootImage("Sad Princess.png");
+        setImage(image);
+    }
+
+    public void act() {
+        
+    }
+    
+}
