c03fe89cc70e9a1de8fa2dee77d4fad7d398acff
fjc-22233580 <22233580@bucks.ac.uk>
2024-01-07 20:25:07 +0000

    Comments.

--- EnemyState.java
+++ EnemyState.java
@@ -1,3 +1,7 @@
+/**
+ * Represents the different states an enemy can be in.
+ * The enemy can be patrolling, chasing, attacking, or returning.
+ */
 public enum EnemyState {
     PATROLLING,
     CHASING,
